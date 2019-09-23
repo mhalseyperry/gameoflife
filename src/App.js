aeea4ef03720a1b59bade9cb540e46a9451a5a5c
Milo Halsey-Perry <milo.halseyperry@gmail.com>
2019-09-23 17:11:53 +0100

    Grid built, grid boxes are seeded randomly and user can select individual boxes.

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import Grid from './components/Grid';
+import './App.css'
+
+class App extends Component {
+    constructor(){
+        super();
+        this.speed = 100;
+        this.rows = 30;
+        this.cols = 50;
+
+        this.state = {
+            generation: 0,
+            gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
+        }
+    }
+
+    selectBox = (row, col) => {
+        let gridCopy = arrayClone(this.state.gridFull);
+        gridCopy[row][col] = !gridCopy[row][col];
+        this.setState({
+            gridFull: gridCopy
+        });
+    }
+
+    seed = () => {
+        let gridCopy = arrayClone(this.state.gridFull);
+        for (let i = 0; i < this.rows; i++){
+            for (let j = 0; j < this.cols; j++){
+                if (Math.floor(Math.random() * 4) === 1) {
+                    gridCopy[i][j] = true;
+                }
+            }
+        }
+        this.setState({
+            gridFull: gridCopy
+        });
+    }
+
+    componentDidMount() {
+        this.seed();
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>The Game Of Life</h1>
+                <Grid
+                    gridFull={this.state.gridFull}
+                    rows={this.rows}
+                    cols={this.cols}
+                    selectBox={this.selectBox}
+                />
+                <p>{this.state.gridFull}</p>
+                <h2>Generations: {this.state.generation}</h2>
+            </div>
+        );
+    }
+}
+
+function arrayClone(arr) {
+    return JSON.parse(JSON.stringify(arr));
+}
+export default App;
